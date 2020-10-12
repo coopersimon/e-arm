32100d9f06084472c4dd47bdd72f06846b279d6f
simon <simon.d.cooper@hotmail.co.uk>
2020-10-12 22:42:40 +0200

    Added more bit tests

--- src/core/armv4_test.rs
+++ src/core/armv4_test.rs
@@ -116,7 +116,7 @@ fn test_and() {
         (
             // AND R1, R0, #0x1F0000: Cond=AL, I=1, Instr=0, S=0, Rn=0, Rd=1, Rot=16, Imm=1F
             TestIn {
-                regs: vec![0x15151515, 1],
+                regs: vec![0x95959595, 1],
                 cpsr: None,
                 instr: 0xE200181F
             },
@@ -127,15 +127,162 @@ fn test_and() {
             }
         ),
         (
-            // ANDS R1, R0, #0: Cond=AL, I=1, Instr=0, S=0, Rn=0, Rd=1, Rot=0, Imm=0
+            // ANDS R1, R0, #0: Cond=AL, I=1, Instr=0, S=1, Rn=0, Rd=1, Rot=0, Imm=0
             TestIn {
-                regs: vec![0x15151515, 1],
+                regs: vec![0x95959595, 1],
                 cpsr: None,
                 instr: 0xE2101000
             },
             TestOut {
                 regs: vec![None, Some(0)],
                 cpsr: Some(CPSR::Z),
+                cycles: None,
+            }
+        )
+    ];
+
+    for (in_data, out_data) in data.iter() {
+        in_data.run_test(out_data);
+    }
+}
+
+#[test]
+fn test_eor() {
+    let data = vec![
+        (
+            // EOR R2, R0, R1: Cond=AL, I=0, Instr=0, S=0, Rn=0, Rd=2, Sh=0, Rm=1
+            TestIn {
+                regs: vec![0x15151515, 0x84848484, 1],
+                cpsr: None,
+                instr: 0xE0202001
+            },
+            TestOut {
+                regs: vec![None, None, Some(0x91919191)],
+                cpsr: Some(CPSR::default()),
+                cycles: None,
+            }
+        ),
+        (
+            // EOR R1, R0, #0x1F0000: Cond=AL, I=1, Instr=0, S=0, Rn=0, Rd=1, Rot=16, Imm=1F
+            TestIn {
+                regs: vec![0x95959595, 1],
+                cpsr: None,
+                instr: 0xE220181F
+            },
+            TestOut {
+                regs: vec![None, Some(0x958A9595)],
+                cpsr: Some(CPSR::default()),
+                cycles: None,
+            }
+        ),
+        (
+            // EORS R1, R0, #0: Cond=AL, I=1, Instr=0, S=1, Rn=0, Rd=1, Rot=0, Imm=0
+            TestIn {
+                regs: vec![0x95959595, 1],
+                cpsr: None,
+                instr: 0xE2301000
+            },
+            TestOut {
+                regs: vec![None, Some(0x95959595)],
+                cpsr: Some(CPSR::N),
+                cycles: None,
+            }
+        )
+    ];
+
+    for (in_data, out_data) in data.iter() {
+        in_data.run_test(out_data);
+    }
+}
+
+#[test]
+fn test_orr() {
+    let data = vec![
+        (
+            // ORR R2, R0, R1: Cond=AL, I=0, Instr=0, S=0, Rn=0, Rd=2, Sh=0, Rm=1
+            TestIn {
+                regs: vec![0x15151515, 0x84848484, 1],
+                cpsr: None,
+                instr: 0xE1802001
+            },
+            TestOut {
+                regs: vec![None, None, Some(0x95959595)],
+                cpsr: Some(CPSR::default()),
+                cycles: None,
+            }
+        ),
+        (
+            // ORR R1, R0, #0x1F0000: Cond=AL, I=1, Instr=0, S=0, Rn=0, Rd=1, Rot=16, Imm=1F
+            TestIn {
+                regs: vec![0x95959595, 1],
+                cpsr: None,
+                instr: 0xE380181F
+            },
+            TestOut {
+                regs: vec![None, Some(0x959F9595)],
+                cpsr: Some(CPSR::default()),
+                cycles: None,
+            }
+        ),
+        (
+            // ORRS R1, R0, #0: Cond=AL, I=1, Instr=0, S=1, Rn=0, Rd=1, Rot=0, Imm=0
+            TestIn {
+                regs: vec![0x95959595, 1],
+                cpsr: None,
+                instr: 0xE3901000
+            },
+            TestOut {
+                regs: vec![None, Some(0x95959595)],
+                cpsr: Some(CPSR::N),
+                cycles: None,
+            }
+        )
+    ];
+
+    for (in_data, out_data) in data.iter() {
+        in_data.run_test(out_data);
+    }
+}
+
+#[test]
+fn test_bic() {
+    let data = vec![
+        (
+            // BIC R2, R0, R1: Cond=AL, I=0, Instr=0, S=0, Rn=0, Rd=2, Sh=0, Rm=1
+            TestIn {
+                regs: vec![0x15151515, 0x84848484, 1],
+                cpsr: None,
+                instr: 0xE1C02001
+            },
+            TestOut {
+                regs: vec![None, None, Some(0x11111111)],
+                cpsr: Some(CPSR::default()),
+                cycles: None,
+            }
+        ),
+        (
+            // BIC R1, R0, #0x1F0000: Cond=AL, I=1, Instr=0, S=0, Rn=0, Rd=1, Rot=16, Imm=1F
+            TestIn {
+                regs: vec![0x95959595, 1],
+                cpsr: None,
+                instr: 0xE3C0181F
+            },
+            TestOut {
+                regs: vec![None, Some(0x95809595)],
+                cpsr: Some(CPSR::default()),
+                cycles: None,
+            }
+        ),
+        (
+            // BICS R1, R0, #0: Cond=AL, I=1, Instr=0, S=0, Rn=0, Rd=1, Rot=0, Imm=0
+            TestIn {
+                regs: vec![0x35353535, 1],
+                cpsr: None,
+                instr: 0xE3D01000
+            },
+            TestOut {
+                regs: vec![None, Some(0x35353535)],
+                cpsr: Some(CPSR::default()),
                 cycles: None,
             }
         )
