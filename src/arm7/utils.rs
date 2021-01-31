197f888ee94bf5cc6d9acbd223b644736bb40fcc
simon <simon@jotta.no>
2021-01-31 13:47:02 +0100

    Fix in to_slice

--- src/arm7/utils.rs
+++ src/arm7/utils.rs
@@ -6,7 +6,7 @@ const NUM_COPROCESSORS: usize = 16;
 
 // TODO: use array when const generics are in rust
 pub fn to_slice(mut coproc: std::collections::HashMap<usize, Box<dyn Coprocessor>>) -> Box<[Option<Box<dyn Coprocessor>>]> {
-    let mut ret = Vec::new();
+    let mut ret = vec![None; 16];
 
     for i in 0..NUM_COPROCESSORS {
         ret[i] = coproc.remove(&i);
